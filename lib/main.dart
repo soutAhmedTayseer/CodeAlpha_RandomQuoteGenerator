df7a210e6e34a97f8c8aecb01de888427ab03c2b
soutAhmedTayseer <122810273+soutAhmedTayseer@users.noreply.github.com>
2024-09-08 17:35:58 +0300

    updating main

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 import 'quote_page.dart';
 
 void main() {
-  runApp(RandomQuoteApp());
+  runApp(const RandomQuoteApp());
 }
 
 class RandomQuoteApp extends StatelessWidget {
+  const RandomQuoteApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Random Quote Generator',
       theme: ThemeData(
         primarySwatch: Colors.blue,
