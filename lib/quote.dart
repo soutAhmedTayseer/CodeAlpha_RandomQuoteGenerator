6121e453e7caeaf36f40cebf0dfde61612dedeca
soutAhmedTayseer <122810273+soutAhmedTayseer@users.noreply.github.com>
2024-09-08 17:36:09 +0300

    updating quotes file

--- lib/quote.dart
+++ lib/quote.dart
@@ -1,14 +1,19 @@
 class Quote {
   final String content;
   final String author;
+  final List<String> tags; // Add tags field
 
-  Quote({required this.content, required this.author});
+  Quote({
+    required this.content,
+    required this.author,
+    required this.tags, // Include tags in constructor
+  });
 
   factory Quote.fromJson(Map<String, dynamic> json) {
     return Quote(
       content: json['content'],
       author: json['author'],
+      tags: List<String>.from(json['tags'] ?? []), // Extract tags from JSON
     );
   }
 }
-
